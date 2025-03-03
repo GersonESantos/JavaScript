e41ac07441438567d92e98a0b79769e7b5e1ed10
GersonESS <gebhsantos@gmail.com>
2025-03-03 13:21:47 -0300

    Update server.js

--- newton/Unidade1/ex3/back/server.js
+++ newton/Unidade1/ex3/back/server.js
@@ -1,6 +1,6 @@
 var http = require("http");
 http.createServer(function (req, res) {
     res.writeHead(200, {"Content-Type": "text/html"});
-    res.end("<h1>Newtom</h1><h2>Ola mundâo</h2>");
+    res.end("<h1>Newtom</h1><h2>Ola mundâo");
 }).listen(3000);
 console.log("Server running at http://localhost:3000/");
