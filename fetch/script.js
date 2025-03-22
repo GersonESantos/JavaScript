97d5355733d31ed8909242907ac5f062dd99616e
GersonESS <gebhsantos@gmail.com>
2025-03-21 23:54:46 -0300

    Update script.js

--- fetch/script.js
+++ fetch/script.js
@@ -1,4 +1,5 @@
-const URL = 'https://dummyjson.com/products';
+// const URL = 'https://dummyjson.com/products';
+const URL = 'http://localhost:3000/user/1'; 
 function chamaapi() {
     fetch(URL)
         .then(response => response.json())
