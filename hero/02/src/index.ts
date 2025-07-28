62026d2c0a038e279a6ed8154c16513ba3663505
GersonESS <gebhsantos@gmail.com>
2025-07-28 01:10:42 -0300

    a

--- hero/02/src/index.ts
+++ hero/02/src/index.ts
@@ -1,1 +1,18 @@
-console.log('Hello, World!');
+class MyFirstClass {
+    name: string;
+    age: number;
+
+  constructor() {
+    this.name = 'GersonES';
+    this.age = 68;
+  }
+  method() {
+    console.log('Hello from MyFirstClass!');
+  }
+}
+const myFirstClass = new MyFirstClass();
+const myName = myFirstClass.name;
+const myAge = myFirstClass.age;
+myFirstClass.method();
+console.log(myName);
+console.log(myAge);
