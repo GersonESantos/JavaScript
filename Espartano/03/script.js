67b836d1fac81dcebb959b33afb0efe34aa9521e
GersonESS <gebhsantos@gmail.com>
2025-07-29 07:02:13 -0300

    x

--- Espartano/03/script.js
+++ Espartano/03/script.js
@@ -1,14 +1,81 @@
 class Produto {
     constructor(nome, preco) {
-        this.id = Math.floor(Math.random() * 1000);
-        this.nome = nome;
-        this.preco = preco;
+        this.id = 1
+        this.arrayProdutos = [];     
     }
-    adicionar() {
-        alert(`Produto ${this.nome} adicionado com sucesso!`);
+    salvar() 
+    {
+        let produto = this.lerDados();
+        if (this.validarCampos(produto)) {
+            this.adicionar(produto);
+        }
+        this.listatabela();
+    
     }
-    excluir() {
-        alert(`Produto ${this.nome} excluído com sucesso!`);
+    listatabela() 
+    {
+        let tbody = document.getElementById('tbody');
+        tbody.innerText = '';
+
+        for (let i = 0; i < this.arrayProdutos.length; i++) {
+            let tr = tbody.insertRow();
+            let td_id = tr.insertCell();
+            let td_nome = tr.insertCell();
+            let td_preco = tr.insertCell();
+            let td_acoes = tr.insertCell();
+
+            td_id.innerText = this.arrayProdutos[i].id;
+            td_nome.innerText = this.arrayProdutos[i].nome;
+            td_preco.innerText = this.arrayProdutos[i].preco;
+            td_id.classList.add('center');
+            let imgEdit = document.createElement('img');
+            imgEdit.src = 'img/iconEditar.png';
+            imgEdit.onclick = function() {
+                produto.editar(i);
+            };
+            td_acoes.appendChild(imgEdit);
+
+            let imgDelete = document.createElement('img');
+            imgDelete.src = 'img/iconDeletar.png';
+            imgDelete.onclick = function() {
+                produto.deletar(i);
+            };
+            td_acoes.appendChild(imgDelete);
+        }
+    }
+
+    validarCampos(produto) {
+        let msg = '';
+        if (produto.nome == '') {
+            msg += 'Informe o nome do produto\n';
+        }
+        if (produto.preco == '') {
+            msg += 'Informe o preço do produto\n';
+        }
+        if (msg != '') {
+            alert(msg);
+            return false;
+        }
+        return true;
+    }
+    adicionar(produto) {
+        this.arrayProdutos.push(produto);
+        this.id++;
+    }
+    lerDados() {
+        let produto = {};
+        produto.id =this.id;
+        produto.nome = document.getElementById('nome').value;
+        produto.preco = document.getElementById('preco').value;
+
+        return {
+            id: produto.id,
+            nome: produto.nome,
+            preco: produto.preco
+        };
+    }
+    cancelar() {
+        alert(`Produto ${this.nome} cancelado com sucesso!`);
     }
 }
 let produto = new Produto("Notebook", 2500);
--- LuizTools/poo/01/index.js
+++ LuizTools/poo/01/index.js
@@ -1,22 +1,21 @@
-const produtos = [
-    { id: 1, nome: "Produto 1", preco: 10.0 , peso: 1.0 },
-    { id: 2, nome: "Produto 2", preco: 20.0 , peso: 2.0 },
-    { id: 3, nome: "Produto 3", preco: 30.0 , peso: 3.0 },
-];
-
-    calcularFrete: (cep) => {
-        // Simulação de cálculo de frete
-        const taxaBase = 5.0;
-        const distancia = cep ? 10 : 0; // Simula uma distância baseada no CEP
-        return taxaBase + (distancia * 0.5);
-    }
-
 const categoria = {
     id: 1,
     nome: "Monitores",
     descricao: "Monitores de alta qualidade",
     banner: "https://example.com/banner.jpg",
     obterDestaque: () => {
-        return produtos.filter(produto => produto.preco > 15.0);
+        return produto;
     },
 };
+const produto = 
+    { id: 1, nome: "Produto 1", preco: 10.0 , peso: 1.0 }
+    
+
+    calcularFrete: (cep, quantidade) => {
+        // Simulação de cálculo de frete
+        const taxaBase = 5.0;
+        const distancia = cep ? 10 : 0; // Simula uma distância baseada no CEP
+        const pesoTotal = produtos.reduce((acc, produto) => acc + (produto.peso * quantidade), 0);
+        return taxaBase + (distancia * 0.5) + (pesoTotal * 0.1);
+    }
+
