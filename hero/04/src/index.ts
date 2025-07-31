5ea24a359cdf9484dd12fc5d90bd1733e4c6a838
GersonESS <gebhsantos@gmail.com>
2025-07-30 22:18:56 -0300

    a

--- hero/04/src/index.ts
+++ hero/04/src/index.ts
@@ -5,50 +5,35 @@ class Aves {
   constructor(especie: string, age: number) {
     this.especie = especie;
     this.age = age;
+   
   }
  
     voar(){
+   
       console.log('voando!');
     }
-    
     comer(){
       console.log('comendo!');
     }
-    
-    // Novos métodos adicionados
-    dormir() {
-      console.log('dormindo!');
+  }
+  
+  class Pato extends Aves {
+    constructor() {
+      super('Pato', 68);
     }
-    
-    cantar() {
-      console.log('cantando!');
+    nadar() {
+      console.log('nadando!');
     }
-    
-    pousar() {
-      console.log('pousando!');
-    }
-    
-    info() {
-      console.log(`Espécie: ${this.especie}, Idade: ${this.age} anos`);
-    }
-    
-    // Método similar ao do exemplo
-    method() {
-      console.log('Hello from Aves!');
-    }
-}
+  }
+const pato = new Pato();
+pato.nadar();
 
 const aves = new Aves('Pato', 68);
 console.log('Aves instance created:', aves);
 aves.voar();
 aves.comer();
-aves.dormir();
-aves.cantar();
-aves.pousar();
-aves.info();
-aves.method(); // Chamando o novo método
-
 const myespecie = aves.especie;
+
 console.log('minha especie ' + myespecie);
 const myAge = aves.age;
-console.log('minha idade ' + myAge);
+console.log('minha idade ' +myAge);
