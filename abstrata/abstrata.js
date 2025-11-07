1e0cb009f4a7c0a2b447f70fa4e772028cb78e38
GersonESS <gebhsantos@gmail.com>
2025-11-07 19:49:02 -0300

    Update abstrata.js

--- abstrata/abstrata.js
+++ abstrata/abstrata.js
@@ -1,24 +1,33 @@
 class Pessoa {
-    constructor(nome, idade) {
-        if (new.target === pessoa) {
+    constructor(nome, idade, curso) {
+        if (new.target === Pessoa) {
             throw new Error("Pessoa é uma classe abstrata e não pode ser instanciada diretamente.");
     }
         this.nome = nome;
         this.idade = idade;
+        this.curso = curso;
     }
     apresentar() {
         throw new Error("Método abstrato 'apresentar' deve ser implementado na subclasse.");
     }       
 }
-// const pessoa1 = new pessoa("João", 30); // Isso lançará um erro
+// const pessoa = new Pessoa("João", 30); // Isso lançará um erro
 
-class pessoa extends Pessoa {
-    constructor(nome, idade, curso) {
-        super(nome, idade);
-        this.curso = curso;
-    }               
+class Professor  extends Pessoa {        
     apresentar() {
         return `Olá, meu nome é ${this.nome}, tenho ${this.idade} anos e estudo ${this.curso}.`;
     }
 }
-const estudante1 = new estudante("Maria", 20, "Engenharia");
+class Aluna extends Pessoa {
+    apresentar() {
+        return `Olá, meu nome é ${this.nome}, tenho ${this.idade} anos e estudo ${this.curso}.`;
+    }
+}
+const professor1 = new Professor("Carlos", 45, "Matemática");
+const aluno1 = new Aluna("Maria", 20, "Engenharia");
+const aluno2 = new Aluna("Ana", 22, "Medicina");
+const pwssoas = [professor1, aluno1, aluno2];
+
+pwssoas.forEach(pessoa => {
+    console.log(pessoa.apresentar());
+});
